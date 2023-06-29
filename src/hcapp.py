37420589c4b8dd556701ce4525c67d32e60ef945
dariuszSki <dariusz.sliwinski@netfoundry.io>
2023-06-29 10:46:01 -0400

    refactored logger

--- src/hcapp.py
+++ src/hcapp.py
@@ -8,9 +8,9 @@ import traceback
 import ipaddress
 import socket
 from datetime import datetime
+import yaml
 import requests
 import urllib3
-import yaml
 from jsonschema import validate, exceptions as jsonexcept
 from colorama import Fore, Style, init
 urllib3.disable_warnings(category = urllib3.exceptions.InsecureRequestWarning)
@@ -54,39 +54,31 @@ def get_arguments():
                         version=__version__)
     return parser.parse_args()
 
-def setup_logging(logfile, loglevel):
+class CustomFormatter(logging.Formatter):
+    """
+    Return a custom color for the message based on the log level.
+    """
+    LEVEL_COLORS = {
+        logging.CRITICAL: Fore.CYAN,
+        logging.ERROR: Fore.RED,
+        logging.WARNING: Fore.YELLOW,
+        logging.INFO: Fore.GREEN,
+        logging.DEBUG: Fore.MAGENTA
+    }
+
+    def format(self, record):
+        level_color = self.LEVEL_COLORS.get(record.levelno, "")
+        colored_levelname = f"{level_color}{record.levelname}{Style.RESET_ALL}"
+        formatted_msg = super().format(record)
+        return formatted_msg.replace(record.levelname, colored_levelname)
+
+def setup_logging(logfile='program_name.log', loglevel=logging.INFO):
     """
     Set up logging to log messages to both the console and a file.
-    Paramters:
-    logfile (string): The file to log messages to. Defaults to 'program_name.log'.
-    loglevel (string): The minimum level of log messages to display. Defaults to logging.INFO.
+    Parameters:
+    - logfile (string): The file to log messages to. Defaults to 'program_name.log'.
+    - loglevel (int or string): The minimum level of log messages to display. Defaults to logging.INFO.
     """
-    class CustomFormatter(logging.Formatter):
-        """
-        Return a custom color for the message if the level is higher than warning.
-        """
-        def format(self, record):
-            if record.levelno == logging.CRITICAL:
-                level_color = Fore.CYAN
-            elif record.levelno == logging.DEBUG:
-                level_color = Fore.MAGENTA
-            elif record.levelno == logging.WARNING:
-                level_color = Fore.YELLOW
-            elif record.levelno >= logging.ERROR:
-                level_color = Fore.RED
-            elif record.levelno == logging.INFO:
-                level_color = Fore.GREEN
-            else:
-                level_color = ""
-
-            formatted_msg = super().format(record)
-            colored_levelname = f"{level_color}{record.levelname}{Style.RESET_ALL}"
-            return formatted_msg.replace(record.levelname, colored_levelname)
-    def console_format(record):
-        if record.levelno == logging.INFO:
-            return console_formatter_info.format(record)
-        return console_formatter_warning_error.format(record)
-
     # Initialize colorama
     init(autoreset=True)
 
@@ -98,25 +90,16 @@ def setup_logging(logfile, loglevel):
     if logfile:
         file_handler = logging.FileHandler(logfile)
         file_handler.setLevel(loglevel)
+        file_formatter = CustomFormatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
 
     # Create a console handler to log messages to the console
     console_handler = logging.StreamHandler()
     console_handler.setLevel(loglevel)
 
-    # Create formatters with custom date and time format, add them to the appropriate handlers
-    file_formatter = CustomFormatter('%(asctime)s-%(levelname)s-%(message)s',
-                                    datefmt='%Y-%m-%d-%H:%M:%S')
-
-    if logfile:
-        file_handler.setFormatter(file_formatter)
-
-    console_formatter_info = CustomFormatter('%(levelname)s-%(message)s')
-    console_formatter_warning_error = CustomFormatter('%(levelname)s-%(message)s')
-
-    console_handler.format = console_format
-
-    if logfile:
-        logger.addHandler(file_handler)
+    console_formatter = CustomFormatter('%(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
 
 def parse_variables(cmdVar, envVar, defaultVar):
@@ -128,7 +111,7 @@ def parse_yaml_file(file, logString):
     logging.debug("Parsing YAML File: %s", file)
 
     if not file:
-        logging.warning("Empty File Path for %s", logString)
+        logging.warning("No File Path given for '%s' file", logString)
         return None
     
     if os.path.getsize(file) == 0:
@@ -139,7 +122,8 @@ def parse_yaml_file(file, logString):
         with open(file, mode='r', encoding='utf-8') as newFile:
             return yaml.safe_load(newFile)
     except (yaml.YAMLError, IOError):
-        logging.warning(traceback.format_exc())
+        logging.warning(traceback.format_exc(0))
+        logging.debug(traceback.format_exc())
 
     return None
 
@@ -192,7 +176,7 @@ def main():
     Main Function
     """
 
-    ### Get command line arguments or environment variables
+    # Get command line arguments or environment variables
     args = get_arguments()
     routerConfigFilePath = parse_variables(args.routerConfigFilePath, 'ROUTER_CONFIG_FILE_PATH',
                                            '/opt/netfoundry/ziti/ziti-router/config.yml')
@@ -204,12 +188,12 @@ def main():
 
     # Set up initial variables' states/values
     setup_logging(logFile, logLevel)
-    if config := parse_yaml_file(routerConfigFilePath, "router config file"):
+    if config := parse_yaml_file(routerConfigFilePath, "router config"):
         pass
     else:
         return 0
     nonTraversableRouters = []
-    if ids := parse_yaml_file(noTFlagRoutersFilePath, "router ids file"):
+    if ids := parse_yaml_file(noTFlagRoutersFilePath, "router ids"):
         try:
             validate(instance=ids, schema=SCHEMA_ROUTERIDS)
             nonTraversableRouters = ids.get("routerIds")
@@ -226,25 +210,27 @@ def main():
         url = f'https://127.0.0.1:{hcPort["address"].split(":")[1]}/{hcPath["binding"]}'
         ctrlAddr = config["ctrl"]["endpoint"].split(":")[1]
     except (KeyError, ValueError):
-        logging.warning(traceback.format_exc())
+        logging.warning(traceback.format_exc(0))
+        logging.debug(traceback.format_exc())
         return 0
     
-    #resolve ctrl dns name if needed
+    # Resolve ctrl dns name if needed
     try:
         ctrlIp = [ctrlAddr] if is_ipv4(ctrlAddr) else [socket.gethostbyname(ctrlAddr)]   
         logging.debug("ctrl address is %s", ctrlIp)
     except (socket.gaierror, socket.herror, OSError):
-        logging.warning(traceback.format_exc())
+        logging.warning(traceback.format_exc(0))
+        logging.debug(traceback.format_exc())
         return 0
 
     # Get Healthcheck data
     try:
         response = requests.get(url, timeout=TIMEOUT, headers=HEADERS, verify=False)
-    except requests.RequestException as err:
-        logging.error(err)
+        hcData = response.json()["data"]
+    except (requests.RequestException, ValueError, KeyError, TypeError):
+        logging.error(traceback.format_exc(0))
+        logging.debug(traceback.format_exc())
         return 1
-
-    hcData = response.json()["data"]
     [controlPingData] = list_comprehension_return_list_if(hcData["checks"],"id","controllerPing")
     [linkHealthData] = list_comprehension_return_list_if(hcData["checks"],"id","link.health")
     logging.debug("HC = %s", hcData)
